14162fb832f6e475d278a7b19a814ef25ffb50ed
Ruslan Karpov <karpovruslan1980@gmail.com>
2022-03-10 23:51:51 +0200

    111

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-const x = 1;
-console.log(x); 
+function multiply(x, y, z) {
+  console.log("Код до return выполняется как обычно");
+
+  // Возвращаем результат выражения умножения
+  return x * y * z;
+
+  console.log("Этот лог никогда не выполнится, он стоит после return");
+}
+
+// Результат работы функции можно сохранить в переменную
+let result = multiply(2, 3, 5);
+console.log(result); // 30
+
+
+
+result = multiply(17, 6, 25);
+console.log(result); // 2550
